760922d61760fe015f319aa44cd0c0ba2fe57384
Muhamad Farhan Rajab <farhanrajab15@gmail.com>
2022-12-06 06:46:18 +0700

    update

--- application/views/templates/template.php
+++ application/views/templates/template.php
@@ -186,10 +186,10 @@
     <!-- ======= Footer ======= -->
 	<footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>Kelompok1</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
-      Designed by <a href="https://bootstrapmade.com/">Kelompok 1</a>
+      Designed by <a href="<?= base_url()?>">Kelompok 1</a>
     </div>
   </footer><!-- End Footer -->
 
